291193f96fa0aa7bf81016bd667f90e52719f805
Ri <ririsabri97@outlook.fr>
2022-01-13 06:01:15 +0100

    confirm-password

--- src/Inputs/Input.jsx
+++ src/Inputs/Input.jsx
@@ -8,7 +8,17 @@ const Input = (props) => {
             <label className='label'>{props.label}</label> 
             <div className='after'>{props.after}</div>
             </div>
-            <input className='input' name={props.name} placeholder={props.placeholder} type={props.type } onChange={props.onChange} required={props.required} pattern={props.pattern} onBlur={(e)=>setFocuse(true)} focused={focused.toString()}/>
+            <input className='input'
+             name={props.name} 
+             placeholder={props.placeholder} 
+             type={props.type } 
+             onChange={props.onChange} 
+             required={props.required} 
+             pattern={props.pattern} 
+             onBlur={(e)=>setFocuse(true)} 
+             focused={focused.toString()}
+             onFocus={()=>props.name==='confirmation'?setFocuse(true):null}
+             />
             <span >{props.errorMessage}</span>
         </div>
     )
